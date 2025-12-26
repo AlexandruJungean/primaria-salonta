23a83bebcad27b7bdbf3223312ac28ccf9a224c2
AlexandruJungean <alex.jungean@gmail.com>
2025-12-26 03:48:19 +0200

    Disable automatic browser locale detection and set Romanian as the default language in routing configuration.

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -9,6 +9,9 @@ export const routing = defineRouting({
 
   // Don't show the locale prefix for the default locale
   localePrefix: 'as-needed',
+
+  // Disable automatic browser locale detection - always use Romanian as default
+  localeDetection: false,
 });
 
 export type Locale = (typeof routing.locales)[number];
